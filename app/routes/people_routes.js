53d42505d4ca41867adbae89055686e9ca299c05
kwebster2 <kevin.r.webster@gmail.com>
2017-05-21 20:45:14 -0600

    setup db

--- app/routes/people_routes.js
+++ app/routes/people_routes.js
@@ -1,6 +1,12 @@
 module.exports = function(app, db) {
   app.post('/people', (req, res) => {
-    console.log(req.body)
-    res.send("Hello")
-  })
+    const person = { name: req.body.name}
+    db.collection('people').insert(person, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occurred' });
+      } else {
+        res.send(result.ops[0]);
+      }
+      });
+  });
 };
