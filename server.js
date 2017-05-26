44a5a0c153fc0d495792db410854948c6426ddd5
kwebster2 <kevin.r.webster@gmail.com>
2017-05-26 15:12:11 -0600

    added cors package

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const db = require('./config/db');
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-require('./app/routes')(app, {});
-
 const port = 8000;
-app.listen(port, () => {
-  console.log('Listening at http://localhost:' + port);
-});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
+
+
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });
+})
