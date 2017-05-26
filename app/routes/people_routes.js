24469bd337294721bc1ed87e17cfece551c68030
kwebster2 <kevin.r.webster@gmail.com>
2017-05-26 15:13:09 -0600

    edit person attributes in post person

--- app/routes/people_routes.js
+++ app/routes/people_routes.js
@@ -1,6 +1,10 @@
 module.exports = function(app, db) {
   app.post('/people', (req, res) => {
-    const person = { name: req.body.name}
+    const person = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      instagram: req.body.instagram
+    }
     db.collection('people').insert(person, (err, result) => {
       if (err) {
         res.send({ 'error': 'An error has occurred' });
